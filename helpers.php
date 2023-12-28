f8943023d5d76a1b7ae4ad84b3d5f2eb9037fca2
Mohit Singh <mohit.singh2@talktalkplc.com>
2023-12-28 10:43:10 +0530

    Creating partials and refactoring code

--- helpers.php
+++ helpers.php
@@ -6,6 +6,39 @@
  * @param string $path
  * @return string
  */
-function basePath(string $path = '') {
+function basePath(string $path = ''): string
+{
     return __DIR__ . '/' . $path;
+}
+
+/**
+ * Loads a view
+ *
+ * @param string $name
+ * @return void
+ */
+function loadView(string $name): void
+{
+    $path = basePath("views/{$name}.view.php");
+    if (file_exists($path)) {
+        require $path;
+    } else {
+        echo "View '{$name}' not found!";
+    }
+}
+
+/**
+ * Loads a partial
+ *
+ * @param string $name
+ * @return void
+ */
+function loadPartial(string $name): void
+{
+    $path = basePath("views/partials/{$name}.php");
+    if (file_exists($path)) {
+        require $path;
+    } else {
+        echo "Partial '{$name}' not found!";
+    }
 }
