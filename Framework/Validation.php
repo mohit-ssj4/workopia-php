17c8d2886ca25692fcb326c8b5b84b76b725ada8
Mohit Singh <mohit.singh2@talktalkplc.com>
2024-01-02 10:31:47 +0530

    Creating a validation class

---
+++ Framework/Validation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Framework;
+
+class Validation
+{
+    /**
+     * Validates a string
+     */
+    public static function string(string $value, int $min = 1, int $max = INF): bool
+    {
+        $length = strlen(trim($value));
+
+        return $length >= $min && $length <= $max;
+    }
+
+    /**
+     * Validates an email
+     */
+    public static function email(string $value): mixed
+    {
+        return filter_var(trim($value), FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * Matches a value against another
+     */
+    public static function match(string $value1, string $value2): bool
+    {
+        return trim($value1) === trim($value2);
+    }
+}
